dbe1c4f6c79c3ee861814c3d553eb2badc1bc60d
Joebert Jacaba <joebertj@gmail.com>
2019-04-22 08:34:05 +0000

    added non-atomic implementation of batch

--- cassandra/batchstatementwrite.py
+++ cassandra/batchstatementwrite.py
@@ -1,13 +1,20 @@
-from cassandra.cluster import Cluster
+from cassandra.cluster import Cluster, BatchStatement
 
 cluster = Cluster()
 session = cluster.connect('user')
 pstmt = session.prepare("INSERT INTO password (shaone,count) VALUES (?,?)")
+i = 0
+batch = BatchStatement()
 f =  open('../test.data','r')
 line = f.readline()
 while line:
+    i += 1
     (shaone,count) = line.split(':')
-    session.execute(pstmt, (shaone,int(count)))
+    batch.add(pstmt, (shaone,int(count)))
     line = f.readline()
+    if i==10000:
+        i = 0
+        session.execute(batch)
+        batch = BatchStatement()
 f.close()
 #551509767
